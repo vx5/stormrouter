0c46583c9d46dd4f14c4d8056312d010e18c6597
The User <actualuser@varuns-mbp.devices.brown.edu>
2019-04-17 16:22:12 -0400

    path skeleton

--- src/main/java/edu/brown/cs/stormrouter/route/Waypoint.java
+++ src/main/java/edu/brown/cs/stormrouter/route/Waypoint.java
@@ -1,5 +1,53 @@
 package edu.brown.cs.stormrouter.route;
 
-public class Waypoint {
+/**
+ * @author vx5
+ *
+ *         Represention of a single waypoint in a path set of directions. This
+ *         is used in the Path class, as paths are represented through lists of
+ *         Waypoints.
+ */
+class Waypoint {
+  // Instance variable to store key attributes of all waypoints
+  private float wayLat;
+  private float wayLong;
+  private int timeToReach;
+  // Instance variables related to weather attributes of waypoint
+  private int weatherType = -1;
+
+  /**
+   * Basic constructor that only requires, stores Waypoint's location.
+   * 
+   * @param newLat  latitude of corresponding waypoint
+   * @param newLong longitude of corresponding waypoint
+   */
+  Waypoint(float newLat, float newLong) {
+    // Stores latitude and longitude
+    wayLat = newLat;
+    wayLong = newLong;
+  }
+
+  float[] getCoords() {
+    // Returns float array containing coordinates
+    return new float[] {
+        wayLat, wayLong
+    };
+  }
+
+  void setTime(int newTime) {
+    timeToReach = newTime;
+  }
+
+  int getTime() {
+    return new Integer(timeToReach);
+  }
+
+  void giveWeather(int newWeatherType) {
+    weatherType = newWeatherType;
+  }
+
+  int getWeather() {
+    return weatherType;
+  }
 
 }
