c90881705c86b25215a5cacf46edd8f08c2152e4
The User <actualuser@varuns-mbp.devices.brown.edu>
2019-04-19 14:23:34 -0400

    PathConverter mostly complete

--- src/main/java/edu/brown/cs/stormrouter/route/PathConverter.java
+++ src/main/java/edu/brown/cs/stormrouter/route/PathConverter.java
@@ -1,5 +1,7 @@
 package edu.brown.cs.stormrouter.route;
 
+import java.util.List;
+
 public final class PathConverter {
   public static final PathConverter inst = new PathConverter();
 
@@ -10,13 +12,16 @@ public final class PathConverter {
     return inst;
   }
 
-  public Path convertPath() {
-    // TODO:
+  public Path convertPath(List<LatLon> inputPath, long unixStartTime) {
     // 1. Take in input path (change parameters)
     // 1a. Make path with offset of 0
-    Path centerPath = new Path(0);
+    Path centerPath = new Path(unixStartTime);
     // 2. Iterate through path, getting information on each point, converting to
     // waypoint, adding to path
+    for (LatLon point : inputPath) {
+      centerPath.addWaypoint(new Waypoint((float) point.getLatitude(),
+          (float) point.getLongitude()));
+    }
     // 3. Return path
     return centerPath;
   }
