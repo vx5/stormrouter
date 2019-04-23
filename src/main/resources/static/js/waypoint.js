e7b5bf3d81335cca000882dd5224e66696471666
Rutchathon Chairattana-apirom <rutchathonchairattana-apirom@rutchathons-mbp.devices.brown.edu>
2019-04-22 22:30:06 -0400

    delete button works

--- src/main/resources/static/js/waypoint.js
+++ src/main/resources/static/js/waypoint.js
@@ -1,37 +1,57 @@
 let numWays = 0;
 const LIMIT = 4;
+let oldDel = null;
+let delway = $("[id='delete-waypoint']");
 
 function addWaypoint(){
-	if(numWays == LIMIT) {
+	if(numWays >= LIMIT) {
 		console.log("waypoint limit: " + LIMIT);
 		return ;
 	}
 	numWays++;
+// 	$(".waypoints").append("<li>\n" + 
+// "              <div class=\"form-row\">\n" + 
+// "                <div class=\"location-field\">\n" + 
+// "                  <label for=\"way-"+ numWays + "\">Waypoint "+numWays+"</label>\n" + 
+// "                  <input id=\"way-"+numWays+"\" name=\"way-"+numWays+"\" type=\"text\">\n" + 
+// "					<i class=\"fas fa-minus\" id=\"delete-waypoint\"></i>"+
+// "                </div>\n" + 
+// "                <div class=\"duration-field\">\n" + 
+// "                  <label for=\"duration-"+numWays+"\">Duration</label>\n" + 
+// "                  <input id=\"duration-"+numWays+"\" name=\"duration-"+numWays+"\" type=\"time\">\n" + 
+// "                </div>\n" + 
+// "              </div>\n" + 
+// "            </li>");
 	$(".waypoints").append("<li>\n" + 
 "              <div class=\"form-row\">\n" + 
 "                <div class=\"location-field\">\n" + 
-"                  <label for=\"way-"+ numWays + "\">Waypoint "+numWays+"</label>\n" + 
-"                  <input id=\"way-"+numWays+"\" name=\"way-"+numWays+"\" type=\"text\">\n" + 
-"					<i class=\"fas fa-minus\" id=\"delete-waypoint\"></i>"+
+"                  <label for=\"way\">Waypoint </label>\n" + 
+"                  <input id=\"way\" name=\"way\" type=\"text\">\n" + 
+"					<i class=\"fas fa-minus\" id=\"delete-waypoint\" onclick='deleteWaypoint(this)'></i>"+
 "                </div>\n" + 
 "                <div class=\"duration-field\">\n" + 
-"                  <label for=\"duration-"+numWays+"\">Duration</label>\n" + 
-"                  <input id=\"duration-"+numWays+"\" name=\"duration-"+numWays+"\" type=\"time\">\n" + 
+"                  <label for=\"duration\">Duration</label>\n" + 
+"                  <input id=\"duration\" name=\"duration\" type=\"time\">\n" + 
 "                </div>\n" + 
 "              </div>\n" + 
 "            </li>");
 }
 
-function deleteWaypoint(){
-	
+
+function deleteWaypoint(elem){
+	console.log($(elem));
+	// console.log($($(elem)[0].parentNode));
+	$($(elem)[0].parentNode)[0].parentNode.remove();
 	numWays--;
 }
 
+delway.click(() => {
+	console.log("delete");
+});
+
 
 $(document).ready(() => {
-	$("#add-waypoint").click(addWaypoint);
-	const delway = $("[id='delete-waypoint']");
-	for(let i = 0; i < delway.length; i++){
-
-	}
+	$("#add-waypoint").click(() => {
+		addWaypoint();
+	});
 });
