0a4b13c6423029c490b19f69acf8f84ad534e7da
The User <actualuser@varuns-mbp.devices.brown.edu>
2019-04-30 11:47:54 -0400

    finished PathWeatherPoint

--- src/main/java/edu/brown/cs/stormrouter/route/PathWeatherPoint.java
+++ src/main/java/edu/brown/cs/stormrouter/route/PathWeatherPoint.java
@@ -2,15 +2,15 @@ package edu.brown.cs.stormrouter.route;
 
 public class PathWeatherPoint {
   // Fields!
-  private float pointLat;
-  private float pointLong;
+  private float lat;
+  private float lon;
   private int weatherType;
   private String weatherSum;
 
   public PathWeatherPoint(float newLat, float newLong, int newType,
       String newSum) {
-    pointLat = newLat;
-    pointLong = newLong;
+    lat = newLat;
+    lon = newLong;
     weatherType = newType;
     weatherSum = newSum;
   }
