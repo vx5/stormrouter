bd4b5e77f72ea32ad341e9285040accd6c0e7710
The User <actualuser@varuns-mbp.devices.brown.edu>
2019-05-03 17:47:11 -0400

    Units, weather scoring rearrangement

--- src/main/java/edu/brown/cs/stormrouter/conversions/Units.java
+++ src/main/java/edu/brown/cs/stormrouter/conversions/Units.java
@@ -3,15 +3,18 @@ package edu.brown.cs.stormrouter.conversions;
 /**
  * @author vx5
  *
- *         Class which stores both constants and methods relevant to interunit
- *         conversion.
+ *         Class which stores both constants and methods relevant to inter-unit
+ *         conversion. Emphasis on time and distance conversion constants, but
+ *         includes a few methods for common, more complex conversions.
  */
 public final class Units {
   // Stores all key unit conversion amounts
   public static final int MS_PER_S = 1000;
   public static final float S_PER_MS = 1 / (float) MS_PER_S;
   public static final int S_PER_MIN = 60;
+  public static final float MIN_PER_S = 1 / (float) S_PER_MIN;
   public static final int MIN_PER_HR = 60;
+  public static final float HR_PER_MIN = 1 / (float) MIN_PER_HR;
   public static final float MILES_PER_DEGREE = 69;
   public static final float DEGREES_PER_MILE = 1 / MILES_PER_DEGREE;
 
