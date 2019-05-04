784d5c3146319b5df925f0aa327e1725e45506f9
Jason Crowley <crowley.json@gmail.com>
2019-05-04 11:14:31 -0400

    add Javadocs for weather package classes

--- src/main/java/edu/brown/cs/stormrouter/weather/WeatherAPIHandler.java
+++ src/main/java/edu/brown/cs/stormrouter/weather/WeatherAPIHandler.java
@@ -8,6 +8,9 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
+/**
+ * Handles retrieving weather from the DarkSky API.
+ */
 public final class WeatherAPIHandler {
   private static final String API_KEY = "b95d19569678f2f58131eb90be980761";
   private static final Gson GSON = new Gson();
@@ -15,6 +18,13 @@ public final class WeatherAPIHandler {
   private WeatherAPIHandler() {
   }
 
+  /**
+   * Gets weather at a certain latitude for the next 48 hours (at most).
+   * @param lat latitude
+   * @param lon longitude
+   * @return the WeatherConditions with HourlyWeather data parsed from DarkSky
+   * @throws Exception If there was an error making or parsing the API request
+   */
   public static WeatherConditions getWeather(double lat, double lon)
       throws Exception {
     String apiString = String
--- src/main/java/edu/brown/cs/stormrouter/weather/WeatherConditions.java
+++ src/main/java/edu/brown/cs/stormrouter/weather/WeatherConditions.java
@@ -1,5 +1,11 @@
 package edu.brown.cs.stormrouter.weather;
 
+/**
+ * All descriptions come from DarkSky's documentation.
+ * https://darksky.net/dev/docs#response-format
+ * <p>
+ * Represents an API response from the DarkSky API.
+ */
 public class WeatherConditions {
   private double latitude;
   private double longitude;
@@ -7,42 +13,75 @@ public class WeatherConditions {
   private TimePoint currently;
   private HourlyWeather hourly;
 
+  /**
+   * @return "The requested latitude."
+   */
   public double getLatitude() {
     return latitude;
   }
 
+  /**
+   * @param latitude to set to
+   */
   public void setLatitude(double latitude) {
     this.latitude = latitude;
   }
 
+  /**
+   * @return "The requested longitude."
+   */
   public double getLongitude() {
     return longitude;
   }
 
+  /**
+   * @param longitude to set to
+   */
   public void setLongitude(double longitude) {
     this.longitude = longitude;
   }
 
+  /**
+   * @return "(e.g. 'America/New_York') The IANA timezone name for the requested
+   * location."
+   */
   public String getTimezone() {
     return timezone;
   }
 
+  /**
+   * @param timezone to set to
+   */
   public void setTimezone(String timezone) {
     this.timezone = timezone;
   }
 
+  /**
+   * @return "A [TimePoint] containing the current weather conditions at the
+   * requested location."
+   */
   public TimePoint getCurrently() {
     return currently;
   }
 
+  /**
+   * @param currently to set to
+   */
   public void setCurrently(TimePoint currently) {
     this.currently = currently;
   }
 
+  /**
+   * @return [An HourlyWeather] containing the weather conditions hour-by-hour
+   * for the next two days."
+   */
   public HourlyWeather getHourly() {
     return hourly;
   }
 
+  /**
+   * @param hourly to set to
+   */
   public void setHourly(HourlyWeather hourly) {
     this.hourly = hourly;
   }
